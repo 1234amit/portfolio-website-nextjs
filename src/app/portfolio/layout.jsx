4f4a08926012172bb9aad6978346693db5f6b07e
Amit Kumar Goswami <cseiubatgoswami@gmail.com>
2023-06-23 12:31:08 +0600

    second commit

--- src/app/portfolio/layout.jsx
+++ src/app/portfolio/layout.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import styles from './page.module.css'
 
-const Layout = () => {
+const Layout = ({children}) => {
   return (
-    <div>Layout</div>
+    <div>
+      <h1 className={styles.mainTitle}>Our Works</h1>
+      {children}
+    </div>
   )
 }
 
